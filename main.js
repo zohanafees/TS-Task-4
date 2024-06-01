fd8cadba892447d8c0dd282e66a165be22c3a859
Zoha Nafees <zohanafees09@gmail.com>
2024-06-01 23:31:55 +0500

    first push

---
+++ main.js
@@ -0,0 +1,6 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Quaid-e-Azam Muhammad Ali jinnah";
+var famous_Quote = "  I donot taking the right decision, i take a decision and make it right.";
+var message = famous_person + " once said " + famous_Quote;
+console.log(message);
+export {};
